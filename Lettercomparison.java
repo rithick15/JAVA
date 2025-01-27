b997449e978c9598c46b5545808b9ffdd98223df
RITHICK K <rithicksiviya@gmail.com>
2025-01-27 21:04:12 +0530

    Add files via upload

---
+++ Lettercomparison.java
@@ -0,0 +1,12 @@
+class Lettercomparison {
+public static void main(String[] args) {
+char letter1 = 'A';
+char letter2 = 'B';
+System.out.println("'" + letter1 + "' < '" + letter2 + "': " + (letter1 < letter2));
+System.out.println("'" + letter1 + "' <= '" + letter2 + "': " + (letter1 <= letter2));
+System.out.println("'" + letter1 + "' > '" + letter2 + "': " + (letter1 > letter2));
+System.out.println("'" + letter1 + "' >= '" + letter2 + "': " + (letter1 >= letter2));
+System.out.println("'" + letter1 + "' == '" + letter2 + "': " + (letter1 == letter2));
+System.out.println("'" + letter1 + "' != '" + letter2 + "': " + (letter1 != letter2));
+}
+}
